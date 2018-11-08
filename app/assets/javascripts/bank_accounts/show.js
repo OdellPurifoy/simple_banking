9da2e2b5d20d33da12a2dfe7c668462a0eca1c17
Odell <odell.purifoy@gmail.com>
2018-11-08 13:41:17 -0500

    Added functionality to the modal controls

--- app/assets/javascripts/bank_accounts/show.js
+++ app/assets/javascripts/bank_accounts/show.js
@@ -4,18 +4,52 @@ var Show = (function() {
   var $btnSave;
   var $inputAmount;
   var $selectTransactionType;
+  var $parameters;
+
+  //non jquery variable
+  var bankAccountId;
 
   var fetchElements = function() {
     $btnNewTransaction = $("#btn-new-transaction");
     $modalTransaction = $("#modal-transaction");
     $btnSave = $("#btn-save");
     $inputAmount = $("#input-amount");
-    $selectTransactionType = "#select-transaction-type";
+    $selectTransactionType = $("#select-transaction-type");
+    $parameters = $("#parameters");
+
+    bankAccountId = $parameters.data("bank-account-id");
+  };
+
+  var disableControls = function() {
+    $btnSave.prop("disabled", true);
+    $inputAmount.prop("disabled", true);
+    $selectTransactionType.prop("disabled", true);
+  };
+
+  var enableControl = function() {
+    $btnSave.prop("disabled", false);
+    $inputAmount.prop("disabled", false);
+    $selectTransactionType.prop("disabled", false);
   };
 
   var initializeEvents = function() {
     $btnNewTransaction.on("click", function() {
       $modalTransaction.modal("show");
+    });
+
+    $btnSave.on("click", function() {
+      var amount = $inputAmount.val();
+      var transactionType = $selectTransactionType.val();
+      disableControls();
+
+      console.log(
+        "Amount " +
+          amount +
+          " Transaction Type" +
+          transactionType +
+          " Bank Account ID:" +
+          bankAccountId
+      );
     });
   };
 
